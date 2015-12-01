3d781c2f2534896254376eed2e066eaa278109ff
Michael Berman <me@scripti.st>
2015-12-01 12:46:06 +0100

    Get multiple lines from /random/:count

--- app/routes.php
+++ app/routes.php
@@ -1,6 +1,16 @@
 <?php
 
-$app->get('/random', function() use($app) {
+$app->get('/random(/:count)', function($count = 1) use($app) {
+	if (!preg_match('/^[0-9]+$/', $count) || intval($count) < 1 || intval($count) > 10)
+		return $app->pass();
+
+	$count = intval($count);
+	if ($count < 1 || $count > 10) {
+		return $app->render(500, [
+			'error' => 'Count must be between 1 and 10'
+		]);
+	}
+
 	global $config, $bible_books;
 	$book = array_rand($config['books']);
 	$bookDb = $config['books'][$book];
@@ -15,33 +25,40 @@ $app->get('/random', function() use($app) {
 		);
 		$db->set_charset($config['dbCredentials']['charset']);
 		$result = $db->query(
-			'SELECT * FROM `' . $bookDb . '` ' .
+			'SELECT *' .
+			' FROM `' . $bookDb . '` ' .
 			'WHERE `blacklisted` = 0 ' .
 			//  Require minimum length of 60 characters
 			'AND LENGTH(text) >= 60 ' .
 			// Require uppercase first letter and full stop at end
 			// 'AND SUBSTRING(text, 1, 1) COLLATE utf8_bin = UPPER(SUBSTRING(text, 1, 1)) ' .
 			// 'AND SUBSTRING(text, -1) = "." ' .
-			'ORDER BY RAND() LIMIT 1'
+			'ORDER BY RAND() LIMIT ' . $count
 		);
 
-		$line = $result->fetch_assoc();
+		if (!$result->num_rows)
+			throw new Exception("Could not get lines", 1);
 
-		// Normalise data
-		if ($book === 'bible') {
-			$line['book'] = $bible_books[$line['book']];
-		} else if ($book === 'quran') {
-			$line['text'] = str_replace('´', '\'', $line['text']);
+		$lines = [];
+
+		while ($line = $result->fetch_assoc()) {
+			array_push($lines, $line);
+
+			// Normalise data
+			if ($book === 'bible') {
+				$line['book'] = $bible_books[$line['book']];
+			} else if ($book === 'quran') {
+				$line['text'] = str_replace('´', '\'', $line['text']);
+			}
 		}
 	} catch (Exception $e) {
 		return $app->render(500, [
-			'msg' => 'Error',
-			'exception' => $e
+			'error' => $e->getMessage()
 		]);
 	}
 
 	$app->render(200, [
-		'line' => $line,
+		'lines' => $lines,
 		'book' => $book,
 	]);
 });
